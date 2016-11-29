1dd53d0dfe7748415cfae2ebc18b3fccdb3ea122
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-11-29 09:49:31 +0100

    Ajout du watch

--- app/ors-stars/ors-stars.js
+++ app/ors-stars/ors-stars.js
@@ -9,20 +9,22 @@
 			restrict: 'EAC',
 			link: function(scope, element, attrs) {
 				console.log('orsStars link', arguments);
-				var note;
-				if (scope[attrs.note] != undefined) {
-					note = scope[attrs.note];
-				} else {
-					note = (attrs.note === undefined) ? 4 : attrs.note;
-				}
-				var html = '';
-				for (var i = 0; i < note; i++) {
-					html += '<img src="ors-stars/img/yellow_star.png" />';
-				}
-				for (var i = note; i < 5; i++) {
-					html += '<img src="ors-stars/img/white_star.png" />';
-				}				
-				element.html(html);
+				scope.$watch('myNote', function() {
+					var note;
+					if (scope[attrs.note] != undefined) {
+						note = scope[attrs.note];
+					} else {
+						note = (attrs.note === undefined) ? 4 : attrs.note;
+					}
+					var html = '';
+					for (var i = 0; i < note; i++) {
+						html += '<img src="ors-stars/img/yellow_star.png" />';
+					}
+					for (var i = note; i < 5; i++) {
+						html += '<img src="ors-stars/img/white_star.png" />';
+					}				
+					element.html(html);
+				});
 			}
 		};
 	});
