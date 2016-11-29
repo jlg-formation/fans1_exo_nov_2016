9209f1ca38bdcb7ea75446f19caef21b51ff798e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-11-29 10:37:07 +0100

    component stars

--- app/ors-stars/ors-stars.js
+++ app/ors-stars/ors-stars.js
@@ -3,38 +3,35 @@
 
 	var app = angular.module('ors-stars', []);
 
-	app.directive('orsStars', ['$compile', function($compile) {
-		return {
-			restrict: 'EAC',
-			scope: {
-				n: '=note'
-			},
-			link: function(scope, element, attrs) {
-				console.log('orsStars link', arguments);
-				scope.update = function(note) {
-					console.log('update', arguments);
-					scope.n = note;
-				};
-				if (attrs.note === undefined) {
-					return;
-				}
-				scope.$watch('n', function() {
-					var note = scope.n;
-					note = Number(note);
-					note = (isNaN(note)) ? 4 : note; 
-					note = (note > 5) ? 5 : note;
-					note = (note < 0) ? 0 : note;
-					var html = '';
-					for (var i = 0; i < note; i++) {
-						html += '<img ng-click="update('+i+')" src="ors-stars/img/yellow_star.png" />';
-					}
-					for (var i = note; i < 5; i++) {
-						html += '<img ng-click="update('+(i+1)+')" src="ors-stars/img/white_star.png" />';
-					}				
-					element.html(html);
-					$compile(element.contents())(scope);
-				});
+	app.component('orsStars', {
+		bindings: {
+			n: '=note'
+		},
+		controller: ['$scope', '$element', '$attrs', '$compile', function($scope, $element, $attrs, $compile) {
+			console.log('orsStars controller', arguments);
+			this.update = function(note) {
+				console.log('update', arguments);
+				this.n = note;
+			};
+			if ($attrs.note === undefined) {
+				return;
 			}
-		};
-	}]);
+			$scope.$watch('$ctrl.n', function() {
+				var note = $scope.$ctrl.n;
+				note = Number(note);
+				note = (isNaN(note)) ? 4 : note; 
+				note = (note > 5) ? 5 : note;
+				note = (note < 0) ? 0 : note;
+				var html = '';
+				for (var i = 0; i < note; i++) {
+					html += '<img ng-click="$ctrl.update('+i+')" src="ors-stars/img/yellow_star.png" />';
+				}
+				for (var i = note; i < 5; i++) {
+					html += '<img ng-click="$ctrl.update('+(i+1)+')" src="ors-stars/img/white_star.png" />';
+				}				
+				$element.html(html);
+				$compile($element.contents())($scope);
+			});
+		}]
+	});
 })();
