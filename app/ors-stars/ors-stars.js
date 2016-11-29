a4d0239e3b8909cc891c8894a885d4fdf5f0ad72
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-11-29 10:22:26 +0100

    Fini

--- app/ors-stars/ors-stars.js
+++ app/ors-stars/ors-stars.js
@@ -3,7 +3,7 @@
 
 	var app = angular.module('ors-stars', []);
 
-	app.directive('orsStars', function() {
+	app.directive('orsStars', ['$compile', function($compile) {
 		return {
 			restrict: 'EAC',
 			scope: {
@@ -11,22 +11,30 @@
 			},
 			link: function(scope, element, attrs) {
 				console.log('orsStars link', arguments);
+				scope.update = function(note) {
+					console.log('update', arguments);
+					scope.n = note;
+				};
+				if (attrs.note === undefined) {
+					return;
+				}
 				scope.$watch('n', function() {
-					var note = (scope.n === undefined) ? 4 : scope.n;
+					var note = scope.n;
 					note = Number(note);
 					note = (isNaN(note)) ? 4 : note; 
 					note = (note > 5) ? 5 : note;
 					note = (note < 0) ? 0 : note;
 					var html = '';
 					for (var i = 0; i < note; i++) {
-						html += '<img src="ors-stars/img/yellow_star.png" />';
+						html += '<img ng-click="update('+i+')" src="ors-stars/img/yellow_star.png" />';
 					}
 					for (var i = note; i < 5; i++) {
-						html += '<img src="ors-stars/img/white_star.png" />';
+						html += '<img ng-click="update('+(i+1)+')" src="ors-stars/img/white_star.png" />';
 					}				
 					element.html(html);
+					$compile(element.contents())(scope);
 				});
 			}
 		};
-	});
+	}]);
 })();
