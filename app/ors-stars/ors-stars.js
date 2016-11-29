ea44bb9d41fcbfa829695f87304b04d76f2baad3
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-11-29 09:58:14 +0100

    Avec les tests et scope

--- app/ors-stars/ors-stars.js
+++ app/ors-stars/ors-stars.js
@@ -1,21 +1,22 @@
 (function() {
 	'use strict';
-	
 
 	var app = angular.module('ors-stars', []);
 
 	app.directive('orsStars', function() {
 		return {
 			restrict: 'EAC',
+			scope: {
+				n: '=note'
+			},
 			link: function(scope, element, attrs) {
 				console.log('orsStars link', arguments);
-				scope.$watch('myNote', function() {
-					var note;
-					if (scope[attrs.note] != undefined) {
-						note = scope[attrs.note];
-					} else {
-						note = (attrs.note === undefined) ? 4 : attrs.note;
-					}
+				scope.$watch('n', function() {
+					var note = (scope.n === undefined) ? 4 : scope.n;
+					note = Number(note);
+					note = (isNaN(note)) ? 4 : note; 
+					note = (note > 5) ? 5 : note;
+					note = (note < 0) ? 0 : note;
 					var html = '';
 					for (var i = 0; i < note; i++) {
 						html += '<img src="ors-stars/img/yellow_star.png" />';
@@ -28,5 +29,4 @@
 			}
 		};
 	});
-	
 })();
