05b68fc7bb3dc01306a334447d46ad8f801a363a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-11-29 16:07:13 +0100

    http $q

--- app/ors-route/ProductCtrl.js
+++ app/ors-route/ProductCtrl.js
@@ -7,11 +7,18 @@
 	app.controller('ProductCtrl', ['$scope', '$injector', function ProductCtrl($scope, $injector) {
 		console.log('ProductCtrl', arguments);
 		var $http = $injector.get('$http');
+		var $q = $injector.get('$q');
 		
 		this.start = function() {
 			console.log('start', arguments);
 			$http.get('/ws/s1').then(function(response) {
-				console.log('s1', response.data);
+				console.log('s1', response);
+				return $q.all([$http.get('/ws/s2'), $http.get('/ws/s3'), $http.get('/ws/s4')]);
+			}).then(function(responses) {
+				console.log('s234', responses);
+				return $http.get('/ws/s5');
+			}).then(function(response) {
+				console.log('s5', response);
 			}).catch(function(error) {
 				console.error('error', error);
 			});
